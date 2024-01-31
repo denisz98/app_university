eec660a19fff07fc98a69452e3c2d95110e51122
denisz98 <deniszaldivarperez98@gmail.com>
2024-01-31 10:42:05 -0500

    añadir: añadi nuevas clases

--- university_app/admin.py
+++ university_app/admin.py
@@ -8,8 +8,16 @@ class Estudiante_admin(admin.ModelAdmin):
     search_fields = ("nombre","telefono")
     list_filter = ("nombre","fecha")
 
+class Clase_admin(admin.ModelAdmin):
+    list_display = ("nombre", "agno")
+    search_fields = ("nombre",)
+    list_filter = ("nombre",)
+
+class Profesor_admin(admin.ModelAdmin):
+    list_display = ("nombre", "carnet_identidad", "telefono")
+    search_fields = ("nombre","telefono","grado_cientifico" )
 
 admin.site.register(Disciplina)
 admin.site.register(Estudiante, Estudiante_admin)
-admin.site.register(Profesor)
-admin.site.register(Clase)
+admin.site.register(Profesor, Profesor_admin)
+admin.site.register(Clase, Clase_admin)
